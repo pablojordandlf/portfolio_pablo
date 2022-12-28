e47cf8bd4a13f617cc8a3f18a35d5b75f67625aa
Pablo Jordán <pablo.jordan.dlf@gmail.com>
2022-12-28 13:52:16 +0100

    more adaptations

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -10,12 +10,12 @@ const Footer = () => {
       <LinkList>
         <LinkColumn>
           <LinkTitle>Call</LinkTitle>
-          <LinkItem href="tel:314-343-3432">314-343-3432</LinkItem>
+          <LinkItem href="tel:660087618">660087618</LinkItem>
         </LinkColumn>
         <LinkColumn>
           <LinkTitle>Email</LinkTitle>
-          <LinkItem href="mailto:contact@jsmastery.com">
-            contact@jsmastery.com
+          <LinkItem href="mailto:pablo.jordan.dlf@gmail.com">
+            pablo.jordan.dlf@gmail.com
           </LinkItem>
         </LinkColumn>
       </LinkList>
@@ -24,15 +24,13 @@ const Footer = () => {
           <Slogan>Innovating one project at a time</Slogan>
         </CompanyContainer>
         <SocialContainer>
-          <SocialIcons href="https://google.com">
+          <SocialIcons href="https://github.com/pablojordandlf">
             <AiFillGithub size="3rem" />
           </SocialIcons>
-          <SocialIcons href="https://google.com">
+          <SocialIcons href="https://www.linkedin.com/in/pablojordan/">
             <AiFillLinkedin size="3rem" />
           </SocialIcons>
-          <SocialIcons href="https://google.com">
-            <AiFillInstagram size="3rem" />
-          </SocialIcons>
+          
         </SocialContainer>
       </SocialIconsContainer>
     </FooterWrapper>
